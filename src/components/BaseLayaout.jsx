6389561f250214fb0d0d0af597da9cf4b6b06224
Miller Arias <millerjavierarias@gmail.com>
2024-05-16 23:18:10 -0500

    Clase Eventos

--- src/components/BaseLayaout.jsx
+++ src/components/BaseLayaout.jsx
@@ -5,6 +5,12 @@ const BaseLayaout = () => {
 	// El hook usado para utilizar maquinas es useMachine del cual declaramos state: que muestra el estado actal de la maquina
 	const [state, send] = useMachine(bookingMachine);
 	console.log(state);
+	// Matches nos indica cual es el estado actual
+	console.log("matches true", state.matches("initial"));
+	console.log("matches false", state.matches("tickets"));
+	// Can nos verifica si podemos ir al estado que se le pase como argumento
+	console.log("can", state.can("FINISH"));
+
 	return <div>Hola</div>;
 };
 
